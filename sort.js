db6c7c138b88d8b0517aeea8973918c33a814235
Aishani Das <aishani2025@gmail.com>
2019-07-22 19:56:53 +0530

    cut implemented with folder name issue resolved, taking last array element after split

--- sort.js
+++ sort.js
@@ -1,32 +1,37 @@
 const fs = require('fs');
 const path = require('path');
-const folderName = "files";
-let dirNames = [];
-fs.readdir(path.join(__dirname, folderName), (err, files) => {
+fs.readdir(__dirname, (err, files) => {
     // console.log(files);
     files.forEach(element => {
-        dir = element.split('.')[1];
-        let src = path.join(__dirname, folderName, element);
-        let destDir = path.join(__dirname, 'sorted', dir);
-        let dest = path.join(destDir, element);
-        if (fs.existsSync(destDir)) {
-            if (fs.existsSync(dest)) {
-                console.log(`${destDir} folder created already\n`);
-                fs.copyFile(src, dest, (err) => {
-                    if (err) throw err;
-                    console.log(`${element} copied into ${destDir}.\n\n`);
+        console.log(`====filename:${element}======`);
+
+        if (fs.statSync(path.join(__dirname, element)).isFile()) {
+            let temparr = element.split('.');
+            let dir = temparr[temparr.length - 1]
+            let src = path.join(__dirname, element);
+            let destDir = path.join(__dirname, 'sorted', dir);
+            let dest = path.join(destDir, element);
+            if (fs.existsSync(destDir)) {
+                if (!fs.existsSync(dest)) {
+                    console.log(`${destDir} folder created already\n`);
+                    fs.copyFile(src, dest, (err) => {
+                        if (err) throw err;
+                        console.log(`${element} copied into ${destDir}.\n\n`);
+                        fs.unlinkSync(src);
+                    });
+                }
+            }
+            else {
+                console.log(`creating folder ${destDir}\n`);
+                fs.mkdir(destDir, { recursive: true }, (err) => {
+                    if (err) throw (err);
+                    fs.copyFile(src, dest, (err) => {
+                        if (err) throw err;
+                        console.log(`${element} copied into after creation ${destDir}.\n\n`);
+                        fs.unlinkSync(src);
+                    });
                 });
             }
-        }
-        else {
-            console.log(`creating folder ${destDir}\n`);
-            fs.mkdir(destDir, { recursive: true }, (err) => {
-                if (err) throw (err);
-                fs.copyFile(src, dest, (err) => {
-                    if (err) throw err;
-                    console.log(`${element} copied into after creation ${destDir}.\n\n`);
-                });
-            });
         }
     });
 });
